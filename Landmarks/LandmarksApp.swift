35aa217ad9d6a54b98916685e0f5f202be335443
Matt2035 <msalern2@nd.edu>
2022-05-23 14:57:00 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Matthew Salerno on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
